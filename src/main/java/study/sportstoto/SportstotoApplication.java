167e88e7a6635e45083fe4fa23fe7da0a577a950
tkddnjs <ksw8864@gmail.com>
2022-11-09 12:23:03 +0900

    First Commit

---
+++ src/main/java/study/sportstoto/SportstotoApplication.java
@@ -0,0 +1,13 @@
+package study.sportstoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportstotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportstotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/sportstoto/SportstotoApplicationTests.java
@@ -0,0 +1,13 @@
+package study.sportstoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportstotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
