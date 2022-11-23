f01633004e5be2e74a272479dfb860410583510d
ms <tokigoki@naver.com>
2022-11-23 09:47:53 +0900

    개발 완료

---
+++ src/main/java/study/sportstoto/controller/TestController.java
@@ -0,0 +1,16 @@
+package study.sportstoto.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class TestController {
+
+  @GetMapping
+  public ResponseEntity<String> getTest() {
+    return ResponseEntity.noContent().build();
+  }
+}
